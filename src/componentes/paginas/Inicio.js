567ffe264129e2d1458a1e511b4e29945d0bf6b1
Bárbara <barbara.neira2015@alu.uct.cl>
2022-05-22 16:54:52 -0400

    Titulo, explicacion y fuente de informacion inicio
    Solo texto, etiquetas relacionadas al titulo, centro de la pagina y fuente

--- src/componentes/paginas/Inicio.js
+++ src/componentes/paginas/Inicio.js
@@ -5,7 +5,21 @@ function Inicio() {
   return (
     <div>
         <Carrusel />
-        <h1>Esta es la pagina de inicio</h1>
+        <div className='container mb-5'>
+            <h1 className='text-white p-3 bg-dark'>Pokemon</h1>
+            <figure>
+                <blockquote>
+                    <p className='text-center mt-3'>
+                    La serie de videojuegos de Pokémon, son videojuegos de rol desarrollados por Game Freak y publicados para videoconsolas portátiles de Nintendo.
+                    La serie se divide en generaciones Pokémon, las cuales son los grupos de videojuegos lanzados en un cierto período y que comparten una serie de 
+                    características similares, concretamente los personajes y criaturas (Pokémon) y la región donde se sucede la historia. Estas generaciones suelen 
+                    corresponderse con las generaciones de las consolas. La aparición sucesiva de generaciones suele tener como consecuencia la publicación de nuevas 
+                    entregas de diversos productos derivados de los videojuegos, como la serie de anime Pokémon. 
+                    </p> 
+                </blockquote>
+                <figcaption className='blockquote-footer'>WIKIPEDIA</figcaption>
+            </figure>
+        </div>
     </div>
   )
 }
